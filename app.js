62d4467bbabaebd13bfffb455e67d701167d8e7d
dheerapat <dheerapat.t@kkumail.com>
2022-11-23 19:51:11 +0700

    express storage with sqlite

---
+++ app.js
@@ -0,0 +1,61 @@
+// import package
+const express = require('express')
+const bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3').verbose()
+//const cors = require('cors')
+require('dotenv').config()
+
+// establish database connection
+const db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log('Database connection established')
+    }
+})
+
+// create table in database
+db.run('CREATE TABLE IF NOT EXISTS addItem (name, type, lotNo, addDate, expiredDate)', (err) => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('Table addItem is ready')
+  }
+})
+
+db.run('CREATE TABLE IF NOT EXISTS retrieveItem (name, retrieveDate)', (err) => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('Table retrieveItem is ready')
+  }
+})
+
+// instantiate package
+const app = express()
+const port = process.env.PORT
+
+// middleware funtion
+app.use(bodyParser.urlencoded({extended: false}))
+//app.use(cors())
+
+// routing
+app.get('/', (req, res) => {
+  res.json({ping: 'pong'})
+})
+
+app.post('/add', (req, res) => {
+
+})
+
+app.post('/retrive', (req, res) => {
+
+})
+
+app.get('/stock/:itemName', (req, res) => {
+  
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
